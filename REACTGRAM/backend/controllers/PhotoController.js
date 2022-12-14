2d94b36a9f1c81943cb6b260228dd8d615618ab9
Leonardo Gama <leogama09@gmail.com>
2022-12-14 14:48:09 -0300

    Update PhotoController.js

--- REACTGRAM/backend/controllers/PhotoController.js
+++ REACTGRAM/backend/controllers/PhotoController.js
@@ -79,8 +79,22 @@ const getAllPhotos = async(req, res) => {
     return res.status(200).json(photos)
 }
 
+// Get user photos
+const getuserPhotos = async(req, res) =>{
+
+    const {id} = req.params
+
+    const photos = await Photo.find({userId: id})
+        .sort([['createdAt', -1]])
+        .exec()
+
+    return res.status(200).json(photos)
+
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
+    getuserPhotos,
 }
