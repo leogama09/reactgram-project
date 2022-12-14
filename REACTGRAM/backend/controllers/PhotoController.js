7e2127d9bda6f876f3cd9198c01a4c17aa671caa
Leonardo Gama <leogama09@gmail.com>
2022-12-14 15:24:34 -0300

    Update PhotoController.js

--- REACTGRAM/backend/controllers/PhotoController.js
+++ REACTGRAM/backend/controllers/PhotoController.js
@@ -108,10 +108,42 @@ const getPhotoById = async (req, res) => {
     res.status(200).json(photo)
 }
 
+// Update a photo
+const updatePhoto = async(req, res) => {
+
+    const {id} = req.params
+    const {title} = req.body
+
+    const reqUser = req.user
+
+    const photo = await Photo.findById(id)
+
+    // Check if photo exists
+    if(!photo) {
+        res.status(404).json({errors: ["Foto não encontrada."]})
+        return
+    }
+
+    // Check if photo belongs to user
+    if(!photo.userId.equals(reqUser._id)) {
+        res.status(422).json({errors: ["Ocorreu um erro, tente novamente mais tarde."]})
+        return
+    }
+
+    if(title) {
+        photo.title = title
+    }
+
+    await photo.save()
+
+    res.status(200).json({ photo, message: "Foto atualizada com sucesso!" })
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getuserPhotos,
     getPhotoById,
+    updatePhoto,
 }
