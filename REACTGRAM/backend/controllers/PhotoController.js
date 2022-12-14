43b20f0b3af59c0444a4dc4bdbfb27723f78a43b
Leonardo Gama <leogama09@gmail.com>
2022-12-14 15:02:02 -0300

    Update PhotoController.js

--- REACTGRAM/backend/controllers/PhotoController.js
+++ REACTGRAM/backend/controllers/PhotoController.js
@@ -92,9 +92,26 @@ const getuserPhotos = async(req, res) =>{
 
 }
 
+// Get photo by id
+const getPhotoById = async (req, res) => {
+
+    const {id} = req.params
+
+    const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+
+    // Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] })
+        return
+    }
+    
+    res.status(200).json(photo)
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getuserPhotos,
+    getPhotoById,
 }
