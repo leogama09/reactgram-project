de6b0c32d6c636664d38f17383d8fa65c030c54f
Leonardo Gama <leogama09@gmail.com>
2022-12-14 10:48:03 -0300

    Update UserRoutes.js

--- REACTGRAM/backend/routes/UserRoutes.js
+++ REACTGRAM/backend/routes/UserRoutes.js
@@ -6,7 +6,8 @@ const {
     register, 
     login, 
     getCurrentUser,
-    update 
+    update, 
+    getUserById
 } = require("../controllers/UserController")
 
 // Middlewares
@@ -24,5 +25,6 @@ router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
 router.get("/profile", authGuard, getCurrentUser)
 router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
+router.get("/:id", getUserById)
 
 module.exports = router
