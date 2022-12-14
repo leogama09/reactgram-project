f689340a82157a75a964f52de68b21194fff248d
Leonardo Gama <leogama09@gmail.com>
2022-12-14 14:48:11 -0300

    Update PhotoRoutes.js

--- REACTGRAM/backend/routes/PhotoRoutes.js
+++ REACTGRAM/backend/routes/PhotoRoutes.js
@@ -2,7 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const { insertPhoto, deletePhoto, getAllPhotos } = require("../controllers/PhotoController")
+const { 
+    insertPhoto, 
+    deletePhoto, 
+    getAllPhotos, 
+    getuserPhotos
+} = require("../controllers/PhotoController")
 
 // Middlewares
 const { photoInsertValidation } = require("../middlewares/photoValidation")
@@ -21,5 +26,6 @@ router.post(
 )
 router.delete("/:id", authGuard, deletePhoto)
 router.get("/", authGuard, getAllPhotos)
+router.get("/user/:id", authGuard, getuserPhotos)
 
 module.exports = router
