7f9794936e428a8d5ccab5b460864e0a61bfa071
Leonardo Gama <leogama09@gmail.com>
2022-12-14 16:46:53 -0300

    Update PhotoController.js

--- REACTGRAM/backend/controllers/PhotoController.js
+++ REACTGRAM/backend/controllers/PhotoController.js
@@ -207,6 +207,17 @@ const commentPhoto = async(req, res) => {
 
 }
 
+// Search photos by title
+const searchPhotos = async(req, res) => {
+
+    const {q} = req.query
+
+    const photos = await Photo.find({title: new RegExp(q, "i")}).exec()
+
+    res.status(200).json(photos)
+
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -216,4 +227,5 @@ module.exports = {
     updatePhoto,
     likePhoto,
     commentPhoto,
+    searchPhotos,
 }
