3c01f646865dbf292c0ec8bab4b161e0c29ea7d7
Leonardo Gama <leogama09@gmail.com>
2022-12-14 15:58:31 -0300

    Update PhotoController.js

--- REACTGRAM/backend/controllers/PhotoController.js
+++ REACTGRAM/backend/controllers/PhotoController.js
@@ -170,6 +170,43 @@ const likePhoto = async(req, res) => {
         .json({ photoId: id, userId: reqUser._id, message: "A foto foi curtida." })
 }
 
+// Comment functionality
+const commentPhoto = async(req, res) => {
+
+    const {id} = req.params
+    const {comment} = req.body
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id)
+
+    const photo = await Photo.findById(id)
+
+    // Check if photo exists
+    if(!photo) {
+        res.status(404).json({errors: ["Foto não encontrada."]})
+        return
+    }
+
+    // Put comment in the array comments
+    const userComment = {
+        comment,
+        userName: user.name,
+        userImage: user.profileImage,
+        userId: user._id
+    }
+
+    photo.comments.push(userComment)
+
+    await photo.save()
+
+    res.status(200).json({
+        comment: userComment,
+        message: "O comentário foi adicionado com sucesso!",
+    })
+
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -178,4 +215,5 @@ module.exports = {
     getPhotoById,
     updatePhoto,
     likePhoto,
+    commentPhoto,
 }
