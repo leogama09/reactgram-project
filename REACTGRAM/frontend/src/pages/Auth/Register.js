06cf2b8f52fae126003f33cc01853884ab14f290
Leonardo Gama <leogama09@gmail.com>
2022-12-15 18:21:09 -0300

    Update Register.js

--- REACTGRAM/frontend/src/pages/Auth/Register.js
+++ REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -5,12 +5,20 @@ import {Link} from 'react-router-dom'
 
 // Hooks
 import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+
+// Redux
+import { register, reset } from "../../slices/authSlice"
 
 const Register = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
+
+  const dispatch = useDispatch()
+
+  const { loading, error } = useSelector((state) => state.auth)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -19,11 +27,18 @@ const Register = () => {
       name,
       email,
       password,
-      confirmPassword
+      confirmPassword,
     }
 
     console.log(user)
+
+    dispatch(register(user))
   }
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset())
+  }, [dispatch])
 
   return (
     <div id="register">
