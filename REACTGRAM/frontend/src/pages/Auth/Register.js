52ca460994d2dfba81fee79667e3b91958d7ac19
Leonardo Gama <leogama09@gmail.com>
2022-12-15 12:27:16 -0300

    Update Register.js

--- REACTGRAM/frontend/src/pages/Auth/Register.js
+++ REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -1,8 +1,33 @@
 import "./Auth.css"
 
+//Components
+import {Link} from 'react-router-dom'
+
+// Hooks
+import { useState, useEffect } from "react"
+
 const Register = () => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
   )
 }
 
