2fbffc81e439675f37c6f2a3b298b170517697be
Leonardo Gama <leogama09@gmail.com>
2022-12-19 18:44:58 -0300

    Update userService.js

--- REACTGRAM/frontend/src/services/userService.js
+++ REACTGRAM/frontend/src/services/userService.js
@@ -44,6 +44,7 @@ const getUserDetails = async (id) => {
             .then((res) => res.json())
             .catch((err) => err)
 
+        return res
     } catch (error) {
         console.log(error)
     }
