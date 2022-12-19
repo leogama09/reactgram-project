db386655b94053a88d769b5afb08fc668033d5cb
Leonardo Gama <leogama09@gmail.com>
2022-12-19 11:29:03 -0300

    Update Login.js

--- REACTGRAM/frontend/src/pages/Auth/Login.js
+++ REACTGRAM/frontend/src/pages/Auth/Login.js
@@ -9,14 +9,31 @@ import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 
 // Redux
+import {login, reset} from '../../slices/authSlice'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const dispatch = useDispatch()
+
+  const {loading, error} = useSelector((state) => state.auth)
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = {
+      email,
+      password,
+    }
+
+    dispatch(login(user))
   }
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset())
+  }, dispatch)
 
   return (
     <div id="login">
@@ -25,7 +42,9 @@ const Login = () => {
       <form onSubmit={handleSubmit}>
       <input type="text" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email || ""} />
       <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""} />
-      <input type="submit" value="Entrar" />
+      {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
       </form>
       <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
     </div>
