b6675ef2829ea0106dcee47dc802e32722f16ede
Leonardo Gama <leogama09@gmail.com>
2022-12-19 10:48:38 -0300

    Update authSlice.js

--- REACTGRAM/frontend/src/slices/authSlice.js
+++ REACTGRAM/frontend/src/slices/authSlice.js
@@ -29,6 +29,20 @@ export const register = createAsyncThunk("auth/register",
         await authService.logout()
     })
 
+    // Sign in an user
+    export const login = createAsyncThunk(
+        "auth/login", 
+        async (user, thunkAPI) => {
+    const data = await authService.login(user)
+
+    // check for errors
+    if(data.errors) {
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+
+    return data
+})
+
     export const authSlice = createSlice({
         name: "auth",
         initialState,
@@ -60,6 +74,21 @@ export const register = createAsyncThunk("auth/register",
                 state.loading = false
                 state.success = true
                 state.error = null
+                state.user = null
+            })
+            .addCase(login.pending, (state) => {
+                state.loading = true
+                state.error = false
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
                 state.user = null
             })
         },
