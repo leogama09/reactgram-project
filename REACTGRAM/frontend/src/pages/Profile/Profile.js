eecef139e8ce830bebea1245a15a949a3f78e5f8
Leonardo Gama <leogama09@gmail.com>
2022-12-20 14:53:14 -0300

    Update Profile.js

--- REACTGRAM/frontend/src/pages/Profile/Profile.js
+++ REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -14,7 +14,7 @@ import { useParams } from "react-router-dom"
 
 // redux
 import { getUserDetails } from "../../slices/userSlice"
-import { publishPhoto, resetMessage } from "../../slices/photoSlice"
+import { publishPhoto, resetMessage, getUserPhotos } from "../../slices/photoSlice"
 
 const Profile = () => {
 
@@ -41,6 +41,7 @@ const Profile = () => {
     // Load user data
     useEffect(() => {
         dispatch(getUserDetails(id))
+        dispatch(getUserPhotos(id))
     }, [dispatch, id])
 
     const handleFile = (e) => {
@@ -117,6 +118,36 @@ const Profile = () => {
             {messagePhoto && <Message msg={messagePhoto} type="success" />}
             </>
         )}
+        <div className="user-photos">
+            <h2>Fotos publicadas:</h2>
+            <div className="photos-container">
+                {photos && 
+                    photos.map((photo) => (
+                    <div className="photo" key={photo._id}>
+                        {photo.image &&(
+                            <img 
+                                src={`${uploads}/photos/${photo.image}`} 
+                                alt={photo.title} 
+                            />
+                        )}
+                        {id === userAuth._id ? (
+                            <div className="actions">
+                                <Link to={`/photos/${photo._id}`}>
+                                    <BsFillEyeFill />
+                                </Link>
+                                <BsPencilFill />
+                                <BsXLg />
+                            </div>
+                        ) : (
+                            <Link className="btn" to={`/photos/${photo._id}`}>
+                                Ver
+                            </Link>
+                        )}
+                    </div>
+                ))}
+                {photos.length === 0 && <p>Ainda não há fotos publicadas</p>}
+            </div>
+        </div>
     </div>
 
 }
