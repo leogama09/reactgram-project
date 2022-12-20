ba65a8cb61c4c38bdb1c3324967f96407ee4afde
Leonardo Gama <leogama09@gmail.com>
2022-12-20 11:50:58 -0300

    Update photoSlice.js

--- REACTGRAM/frontend/src/slices/photoSlice.js
+++ REACTGRAM/frontend/src/slices/photoSlice.js
@@ -10,7 +10,23 @@ const initialState = {
     message: null
 }
 
-//funcoes
+// Publish user photo
+export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async(photo, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token
+
+        const data = await photoService.publishPhoto(photo, token)
+
+        // Check for errors
+        if(data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -19,6 +35,26 @@ export const photoSlice = createSlice({
         resetMessage: (state) => {
             state.message = null
         },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(publishPhoto.pending, (state) => {
+                state.loading = true
+                state.error = false
+            })
+            .addCase(publishPhoto.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.photo = action.payload
+                state.photos.unshift(state.photo)
+                state.message = "Foto publicada com sucesso!"
+            })
+            .addCase(publishPhoto.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+                state.photo = {}
+            })
     },
 })
 
