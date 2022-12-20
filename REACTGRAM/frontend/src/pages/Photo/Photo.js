d3ed9e0c53af98243593c651fef022a83f0a0798
Leonardo Gama <leogama09@gmail.com>
2022-12-20 18:27:50 -0300

    Update Photo.js

--- REACTGRAM/frontend/src/pages/Photo/Photo.js
+++ REACTGRAM/frontend/src/pages/Photo/Photo.js
@@ -5,6 +5,7 @@ import { uploads } from "../../utils/config"
 // components
 import Message from "../../components/Message"
 import { Link } from "react-router-dom"
+import PhotoItem from "../../components/PhotoItem"
 
 // hooks
 import { useEffect, useState } from "react"
@@ -12,10 +13,33 @@ import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 
 // Redux
+import { getPhoto } from "../../slices/photoSlice"
 
 const Photo = () => {
+    const {id} = useParams()
+
+    const dispatch = useDispatch()
+
+    const {user} = useSelector((state) => state.auth)
+    const {photo, loading, error, message} = useSelector((state) => state.photo)
+
+    // comentários
+
+    // Load photo data
+    useEffect(() => {
+        dispatch(getPhoto(id))
+    }, [dispatch, id])
+
+    // like e comentário
+
+    if(loading) {
+        return <p>Carregando...</p>
+    }
+
   return (
-    <div>Photo</div>
+    <div id="photo">
+        <PhotoItem photo={photo} />
+    </div>
   )
 }
 
