6a41b0cccad731d6c663871d849dea347cbec1f1
Leonardo Gama <leogama09@gmail.com>
2022-12-20 16:36:32 -0300

    Update

---
+++ REACTGRAM/frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,22 @@
+import "./Photo.css"
+
+import { uploads } from "../../utils/config"
+
+// components
+import Message from "../../components/Message"
+import { Link } from "react-router-dom"
+
+// hooks
+import { useEffect, useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
+
+// Redux
+
+const Photo = () => {
+  return (
+    <div>Photo</div>
+  )
+}
+
+export default Photo
