0b5b08cf78c99c913c4caad454cb78b01adf4dcc
Leonardo Gama <leogama09@gmail.com>
2022-12-20 17:56:45 -0300

    Update photoSlice.js

--- REACTGRAM/frontend/src/slices/photoSlice.js
+++ REACTGRAM/frontend/src/slices/photoSlice.js
@@ -82,6 +82,14 @@ export const updatePhoto = createAsyncThunk(
     }
 )
 
+// Get photo by id
+export const getPhoto = createAsyncThunk("photo//getphoto",
+async(id) => {
+    const data = await photoService.getPhoto(id)
+
+    return data
+})
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -161,6 +169,16 @@ export const photoSlice = createSlice({
                 state.loading = false
                 state.error = action.payload
                 state.photo = {}
+            })
+            .addCase(getPhoto.pending, (state) => {
+                state.loading = true
+                state.error = false
+            })
+            .addCase(getPhoto.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.photo = action.payload
             })
             
     },
