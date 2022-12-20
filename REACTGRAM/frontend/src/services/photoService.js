fb09c73805218d26a4b5532d3fa476f4ffe1c853
Leonardo Gama <leogama09@gmail.com>
2022-12-20 12:34:49 -0300

    Update photoService.js

--- REACTGRAM/frontend/src/services/photoService.js
+++ REACTGRAM/frontend/src/services/photoService.js
@@ -6,7 +6,6 @@ const publishPhoto = async(data, token) => {
     const config = requestConfig("POST", data, token, true)
 
     try {
-        
         const res = await fetch(api + "/photos", config)
             .then((res) => res.json())
             .catch((err) => err)
@@ -19,12 +18,10 @@ const publishPhoto = async(data, token) => {
 }
 
 // Get user photos
-const getUserPhotos = async(id) => {
-
-    const config = requestConfig("GET")
+const getUserPhotos = async(id, token) => {
+    const config = requestConfig("GET", null, token)
 
     try {
-
         const res = await fetch(api + "/photos/user/" + id, config)
             .then((res) => res.json())
             .catch((err) => err)
