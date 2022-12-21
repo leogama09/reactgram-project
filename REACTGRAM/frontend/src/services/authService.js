a211a36aaf8dba717ec93dca2d80d8cefd17e26a
Leonardo Gama <leogama09@gmail.com>
2022-12-21 15:27:23 -0300

    Update authService.js

--- REACTGRAM/frontend/src/services/authService.js
+++ REACTGRAM/frontend/src/services/authService.js
@@ -1,56 +1,52 @@
-import {api, requestConfig} from '../utils/config'
+import { api, requestConfig } from "../utils/config";
 
-// Register an user
-const register = async(data) => {
-    const config = requestConfig("POST", data)
+// Register a user
+const register = async (data) => {
+  const config = requestConfig("POST", data);
 
-    try {
-        const res = await fetch(api + "/users/register", config)
-            .then((res) => res.json())
-            .catch((err) => err)
+  try {
+    const res = await fetch(api + "/users/register", config)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-        if(res._id) {
-            localStorage.setItem("user", JSON.stringify(res))
-        }
-
-        return res
-    } catch (error) {
-        console.log(error)
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
     }
 
-}
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// Logout an user
+// Logout a user
 const logout = () => {
-    localStorage.removeItem("user")
-}
+  localStorage.removeItem("user");
+};
 
-// Sign in an user
-const login = async(data) => {
+// Sign in a user
+const login = async (data) => {
+  const config = requestConfig("POST", data);
 
-    const config = requestConfig("POST", data)
+  try {
+    const res = await fetch(api + "/users/login", config)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-    try {
-
-        const res = await fetch(api + "/users/login", config)
-            .then((res) => res.json())
-            .catch((err) => err)
-
-    if(res._id) {
-        localStorage.setItem("user", JSON.stringify(res))
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
     }
 
-        return res
-    } catch (error) {
-        console.log(error)
-    }
-
-}
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const authService = {
-    register,
-    logout,
-    login,
-}
+  register,
+  logout,
+  login,
+};
 
-export default authService
+export default authService;
