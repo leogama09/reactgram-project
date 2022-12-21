48acff39b5c830ebe4c9175949ce8ecfe36deeba
Leonardo Gama <leogama09@gmail.com>
2022-12-21 11:59:14 -0300

    Update Photo.js

--- REACTGRAM/frontend/src/pages/Photo/Photo.js
+++ REACTGRAM/frontend/src/pages/Photo/Photo.js
@@ -11,6 +11,7 @@ import PhotoItem from "../../components/PhotoItem"
 import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage"
 
 // Redux
 import { getPhoto, like } from "../../slices/photoSlice"
@@ -20,6 +21,8 @@ const Photo = () => {
     const {id} = useParams()
 
     const dispatch = useDispatch()
+
+    const resetMessage = useResetComponentMessage(dispatch)
 
     const {user} = useSelector((state) => state.auth)
     const {photo, loading, error, message} = useSelector((state) => state.photo)
@@ -33,6 +36,8 @@ const Photo = () => {
 
     const handleLike = () => {
       dispatch(like(photo._id))
+
+      resetMessage()
     }
 
     if(loading) {
@@ -43,6 +48,10 @@ const Photo = () => {
     <div id="photo">
         <PhotoItem photo={photo} />
         <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+        <div className="message-container">
+          {error && <Message msg={error} type="error" />}
+          {message && <Message msg={message} type="success" />}
+        </div>
     </div>
   )
 }
